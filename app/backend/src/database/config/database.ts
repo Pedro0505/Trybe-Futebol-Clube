4072d8a549cf23e30186b1c273479a1ea80c54e5
pedro <pedrohrds05@hotmail.com>
2022-08-10 22:17:29 -0300

    refactor: remove trybe copy rigth

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -3,10 +3,10 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  password: process.env.DB_PASS || 'password',
   database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
