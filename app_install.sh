28a143fe7990ab811444ee9958cf6ec7fc34f5bd
pedro <pedrohrds05@hotmail.com>
2022-08-15 21:19:11 -0300

    feat: add a shell script to install dependencies

---
+++ app_install.sh
@@ -0,0 +1,5 @@
+printf "Instalando as dependecias do backend\n"
+npm install --silent --prefix app/backend
+
+printf "Instalando as dependecias do frontend\n"
+npm install --silent --prefix app/frontend
