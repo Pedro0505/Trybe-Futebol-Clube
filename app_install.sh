fad43f386cdb5a1325d8e846e9a197be82f859f4
pedro <pedrohrds05@hotmail.com>
2022-08-16 08:44:54 -0300

    fix: chage path to execute npm install in shell script

--- app_install.sh
+++ app_install.sh
@@ -1,5 +1,7 @@
 printf "Instalando as dependecias do backend\n"
-npm install --silent --prefix app/backend
+npm install --silent --prefix ./backend
+printf "Terminou a instalação do backend\n"
 
 printf "Instalando as dependecias do frontend\n"
-npm install --silent --prefix app/frontend
+npm install --silent --prefix ./frontend
+printf "Tudo pronto!!\n"
